d730aa74b02233b1b8031ffc5851bd36c2b2d0fe
raaj648 <raajkhan000648@gmail.com>
2025-11-22 01:33:30 +0600

    Update script.js

--- script.js
+++ script.js
@@ -8,15 +8,15 @@ document.addEventListener("DOMContentLoaded", () => {
   const OFFERS_BY_COUNTRY = {
     "US": [
         "https://amzn.to/44dBQJe", 
-        "https://amzn.to/55555", 
-        "https://amzn.to/8888"
+        "https://amzn.to/44dBQJe", 
+        "https://amzn.to/44dBQJe"
     ],
     "GB": [
         "https://amzn.to/44dBQJe", 
-        "https://amzn.to/999"
+        "https://amzn.to/44dBQJe"
     ],
     "CA": [
-        "https://amzn.to/441111"
+        "https://amzn.to/44dBQJe"
     ],
     "BR": [
         "https://1wksrw.com/?open=register&p=h8zt"
@@ -32,7 +32,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const CROSS_BORDER_RULES = {
     "https://1wksrw.com/betting?open=register&p=xctu": ["BR", "RU", "IN"], // This link also works in Brazil, Russia, India 
     "https://1wksrw.com/?open=register&p=h8zt": ["BD", "PH", "AR"],  // This link also works in Bangladesh, Philipines, Argentina
-    "https://record.betsson.com/_Ipto0Q-i5zR7HLc7-ZUbAGNd7ZgqdRLk/1/": ["AR", "BR", "CO", "GR", "BD"]
+    "https://record.betsson.com/_Ipto0Q-i5zR7HLc7-ZUbAGNd7ZgqdRLk/1/": ["AR", "BR", "CO", "GR"]
   };
 
   // --- NEW: HIGH TRAFFIC GEO-DETECTION (GITHUB PAGES OPTIMIZED - OPTION A) ---
@@ -381,4 +381,5 @@ document.addEventListener("DOMContentLoaded", () => {
 
   initializePage();
 });
+
 
