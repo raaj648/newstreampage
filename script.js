adfbb15db2d56967d4c96a0f74833ea8bfa94a9c
raaj648 <raajkhan000648@gmail.com>
2025-11-22 00:55:46 +0600

    Update script.js

--- script.js
+++ script.js
@@ -279,6 +279,8 @@ document.addEventListener("DOMContentLoaded", () => {
       const channelName = getChannelName(channelUrl, index);
       const link = document.createElement("a");
       link.className = "stream-link";
+      link.target = "_blank";
+      link.rel = "noopener noreferrer";
       link.href = `${window.location.origin}${window.location.pathname}?id=${matchId}&stream=${encodeURIComponent(channelUrl)}`;
 
       let buttonText = "Switch";
