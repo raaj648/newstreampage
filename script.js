a513f53e415f04a7702a70d038b0a7a9820023f8
raaj648 <raajkhan000648@gmail.com>
2025-11-22 14:26:06 +0600

    Update script.js

--- script.js
+++ script.js
@@ -7,35 +7,33 @@ document.addEventListener("DOMContentLoaded", () => {
   // PART A: Base Lists per Country
   const OFFERS_BY_COUNTRY = {
     "US": [
-        "https://amzn.to/44dBQJe", 
-        "https://amzn.to/44dBQJe", 
-        "https://amzn.to/44dBQJe"
+        "https://record.webpartners.co/_QUm2k2WIfIo1bkozPnn1SmNd7ZgqdRLk/1/", 
+        "https://record.webpartners.co/_QUm2k2WIfIoBfkWNuc6vQmNd7ZgqdRLk/1/"
     ],
     "GB": [
         "https://amzn.to/44dBQJe", 
         "https://amzn.to/44dBQJe"
     ],
     "CA": [
-        "https://record.betsson.com/_Ipto0Q-i5zSwcAgXsjz1uGNd7ZgqdRLk/1/",
-        "https://record.betsson.com/_Ipto0Q-i5zSMxelRx-4u3GNd7ZgqdRLk/1/",
-        "https://record.betsson.com/_Ipto0Q-i5zQMGjCE5hF4R2Nd7ZgqdRLk/1/"
+         "https://record.betsson.com/_Ipto0Q-i5zSwcAgXsjz1uGNd7ZgqdRLk/1/",
+        "https://record.webpartners.co/_QUm2k2WIfIo1bkozPnn1SmNd7ZgqdRLk/1/"
     ],
     "BR": [
         "https://1wksrw.com/?open=register&p=h8zt"
     ],
     // Fallback for everyone else
     "Global": [
-        "https://1wksrw.com/betting?open=register&p=xctu",
+        "https://www.effectivegatecpm.com/mwvtqzkj?key=daf26372fb52174ce25eca816951dd61",
         "https://1wksrw.com/?open=register&p=h8zt"
     ]
   };
 
   // PART B: Cross-Border Rules
   const CROSS_BORDER_RULES = {
-    "https://1wksrw.com/betting?open=register&p=xctu": ["BR", "RU", "IN"], // This link also works in Brazil, Russia, India 
-    "https://1wksrw.com/?open=register&p=h8zt": ["BD", "PH", "AR"],  // This link also works in Bangladesh, Philipines, Argentina
-    "https://record.betsson.com/_Ipto0Q-i5zR7HLc7-ZUbAGNd7ZgqdRLk/1/": ["AR", "BR", "CO", "GR"]
-  };
+    "https://1wksrw.com/betting?open=register&p=xctu": ["BR", "RU", "IN", "GR", "PH", "IE", "IT", "TR", "ID", "PK"],
+    "https://record.betsson.com/_Ipto0Q-i5zR7HLc7-ZUbAGNd7ZgqdRLk/1/": ["AR", "BR", "CO", "EE", "ES", "KZ", "LV", "LT", "MX", "CL", "PE", "RS", "HR"],
+    "https://record.webpartners.co/_QUm2k2WIfIo1bkozPnn1SmNd7ZgqdRLk/1/": ["DE", "NO", "NZ", "PR", "PT", "AR", "CL", "PH", "SG", "HR"]
+};
 
   // --- NEW: HIGH TRAFFIC GEO-DETECTION (GITHUB PAGES OPTIMIZED - OPTION A) ---
   async function getHighTrafficCountry() {
@@ -383,6 +381,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
   initializePage();
 });
+
 
 
 
